e840cd3cc23940ed320e016f6779ad2853bab4c5
Steven Wilcox <covidtutoring@gmail.com>
2020-11-19 20:19:51 -0500

    feat(root): heroku deploy plus 'homepage' test endpoint

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -2,6 +2,11 @@ from flask import Blueprint, request
 
 prediction_app = Blueprint('prediction_app', __name__)
 
+@prediction_app.route('/', methods=['GET'])
+def health():
+    if request.method == 'GET':
+        return 'Home Page'
+
 @prediction_app.route('/health', methods=['GET'])
 def health():
     if request.method == 'GET':
