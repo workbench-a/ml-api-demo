2fadd7f57d87ba70eac6d894fcae7506f9582b81
Steven Wilcox <covidtutoring@gmail.com>
2020-11-19 20:44:59 -0500

    fix(api): gunicorn port to 8000

--- packages/ml_api/api/config.py
+++ packages/ml_api/api/config.py
@@ -9,14 +9,14 @@ class Config:
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'change-this-not-so-super-secure-key'
-    SERVER_PORT = 5000
+    SERVER_PORT = 8000
     UPLOAD_FOLDER = UPLOAD_FOLDER
 
 class ProductionConfig(Config):
     DEBUG = False
     # should this be configured 'more specifically?'
     SERVER_ADDRESS: os.environ.get('SERVER_ADDRESS', '0.0.0.0')
-    SERVER_PORT: os.environ.get('SERVER_PORT', '5000')
+    SERVER_PORT: os.environ.get('SERVER_PORT', '8000')
 
 
 class DevelopmentConfig(Config):
--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -7,4 +7,4 @@ application = create_app(
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port='5000')
+    application.run()
