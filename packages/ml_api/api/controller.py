3378c9ed2c3090ef7fc338e276cb3776091f04fd
Steven Wilcox <covidtutoring@gmail.com>
2020-11-21 00:16:43 -0500

    feat(packages/regression_demo): minimal regression pipeline without prediction api endpoint

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -2,12 +2,12 @@ from flask import Blueprint, request
 
 prediction_app = Blueprint('prediction_app', __name__)
 
-@prediction_app.route('/', methods=['GET'])
-def home():
-    if request.method == 'GET':
-        return 'Home Page'
-
 @prediction_app.route('/health', methods=['GET'])
 def health():
     if request.method == 'GET':
         return 'ok'
+
+@prediction_app.route('/', methods=['GET'])
+def home():
+    if request.method == 'GET':
+        return 'Home Page'
