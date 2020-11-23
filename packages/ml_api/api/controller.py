165a9a5797945b4badc4e8715e6418a049aabd40
Steven Wilcox <covidtutoring@gmail.com>
2020-11-22 19:07:54 -0500

    fix(root): getting heroku deployment working

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -16,29 +16,29 @@ def home():
     if request.method == 'GET':
         return 'Home Page'
 
-@prediction_app.route('/version', methods=['GET'])
-def version():
-    if request.method == 'GET':
-        return jsonify({'model_version': _version,
-                        'api_version': api_version})
+# @prediction_app.route('/version', methods=['GET'])
+# def version():
+#     if request.method == 'GET':
+#         return jsonify({'model_version': _version,
+#                         'api_version': api_version})
 
 
-@prediction_app.route('/v1/predict/regression', methods=['GET'])
-def predict():
-    if request.method == 'GET':
-        # Step 1: Extract POST data from request body as JSON
-        input_data = load_dataset(mode="production", file_name="test.csv")
-        result = make_prediction(input_data=input_data)
+# @prediction_app.route('/v1/predict/regression', methods=['GET'])
+# def predict():
+#     if request.method == 'GET':
+#         # Step 1: Extract POST data from request body as JSON
+#         input_data = load_dataset(mode="production", file_name="test.csv")
+#         result = make_prediction(input_data=input_data)
 
-        # Step 4: Convert numpy ndarray to list
-        predictions = result.get('predictions').tolist()
-        version = result.get('version')
-        errors = ''
+#         # Step 4: Convert numpy ndarray to list
+#         predictions = result.get('predictions').tolist()
+#         version = result.get('version')
+#         errors = ''
 
-        # Step 5: Return the response as JSON
-        return jsonify({'predictions': predictions,
-                        'version': version,
-                        'errors': errors})
+#         # Step 5: Return the response as JSON
+#         return jsonify({'predictions': predictions,
+#                         'version': version,
+#                         'errors': errors})
 
 # @prediction_app.route('/v1/predict/regression', methods=['POST'])
 # def predict():
